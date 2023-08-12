7d7a0d76f4ff1abd53d67db5debc6afb3adc28a3
Austin White <austinlldb@gmail.com>
2023-08-12 18:58:13 -0500

    qr component

---
+++ script.js
@@ -0,0 +1,5 @@
+let qr_img = document.getElementById("qr-img");
+qr_img.setAttribute(
+  "src",
+  "https://api.qrserver.com/v1/create-qr-code/?data=www.frontendmentor.io/&bgcolor=3685fe&color=ffffff"
+);
